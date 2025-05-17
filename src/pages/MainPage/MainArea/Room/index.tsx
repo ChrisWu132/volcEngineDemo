53ebb32a3e80650d81b51dd60bc5e53b18575cf0
Haipeng Wu <wuhaipen@usc.edu>
2025-05-17 16:31:48 -0700

    room前端大致修改完成

--- src/pages/MainPage/MainArea/Room/index.tsx
+++ src/pages/MainPage/MainArea/Room/index.tsx
@@ -11,16 +11,32 @@ import AudioController from './AudioController';
 import utils from '@/utils/utils';
 import style from './index.module.less';
 import MONDI_COUNSELOR from '@/assets/img/MONDI_COUNSELOR.png';
+import MainBackground from '@/assets/img/main_background.png';
+import RobotImage from '@/assets/img/robot.png';
 
 function Room() {
   return (
     <div className={`${style.wrapper} ${utils.isMobile() ? style.mobile : ''}`}>
-      <AvatarCard avatar={MONDI_COUNSELOR} className={style.avatar} />
-      {utils.isMobile() ? null : <CameraArea />}
-      <Conversation className={style.conversation} />
-      <ToolBar className={style.toolBar} />
-      <AudioController className={style.controller} />
-      <div className={style.declare}>AI生成内容由大模型生成，不能完全保障真实</div>
+      {/* Background container */}
+      <div className={style.backgroundContainer}>
+        <img src={MainBackground} alt="背景" className={style.background} />
+      </div>
+      
+      {/* Content container */}
+      <div className={style.contentContainer}>
+        <AvatarCard avatar={MONDI_COUNSELOR} className={style.avatar} />
+        {utils.isMobile() ? null : <CameraArea />}
+        
+        {/* Robot image */}
+        <div className={style.robotContainer}>
+          <img src={RobotImage} alt="机器人" className={style.robot} />
+        </div>
+        
+        <Conversation className={style.conversation} />
+        <ToolBar className={style.toolBar} />
+        <AudioController className={style.controller} />
+        <div className={style.declare}>AI生成内容由大模型生成，不能完全保障真实</div>
+      </div>
     </div>
   );
 }
